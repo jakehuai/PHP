468a29997feb35596fea4fc5b98b66b708127b76
jakehuai <jack_1997@live.com.my>
2017-06-14 17:45:11 +0800

    Create savecarthw.php

--- savecarthw.php
+++ savecarthw.php
@@ -1,10 +1,10 @@
 <?php
 session_start();
 if(isset($_SESSION["ID"])){
-	$id=$_SESSION["ID"];
-	$name=$_SESSION["name"];
-	$price=$_SESSION["price"];
-	$quantity=$_SESSION["quantity"];
+		$id=$_SESSION["ID"];
+		$name=$_SESSION["name"];
+		$price=$_SESSION["price"];
+		$quantity=$_SESSION["quantity"];
 	setcookie($id."[id]",$id,time()+3600);
 	setcookie($id."[name]",$name,time()+3600);
 	setcookie($id."[price]",$price,time()+3600);
