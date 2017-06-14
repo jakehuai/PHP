66cf796071fca14baa78e52900a6be4844ac1c7d
jakehuai <jack_1997@live.com.my>
2017-06-14 17:46:19 +0800

    Create shoppingcarthw.php

--- shoppingcarthw.php
+++ shoppingcarthw.php
@@ -2,25 +2,27 @@
 $total=0;
 while( list($arr,$value) = each($_COOKIE)){
 	if(isset($_COOKIE[$arr])&&is_array($_COOKIE[$arr])){
+		
 	echo "<a href='deletehw.php?id=$arr'>刪除</a>";
 	while(list($name,$value) = each($_COOKIE[$arr])){
+		
 		if($name=="name"){
-			echo $value." ";
+				echo $value." ";
 		}
-		if($name=="price"){
+			if($name=="price"){
 			$price=$value;
-			echo $value," ";
+				echo $value," ";
 		}
-		if($name=="quantity"){
+			if($name=="quantity"){
 			$quantity=$value;
-			echo $value," ";
+				echo $value," ";
 		}
 		
 	}
-	$total+=$price * $quantity;
+		$total+=$price * $quantity;
 		echo "<br>";
 	}
 }
-	echo "總價:",$total,"<br>";
-	echo "<a href='cataloghw.php'>商品目錄</a>";
+		echo "總共價格:",$total,"<br>";
+		echo "<a href='cataloghw.php'>商品der目錄</a>";
 ?>
